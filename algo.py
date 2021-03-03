9344a3778105068ae544c686fc194a7e6047ecc7
nick adams <thebigdeal916@icloud.com>
2021-03-03 17:24:17 -0600

    lists added

---
+++ algo.py
@@ -0,0 +1,41 @@
+                 #####      PYTHON ALGORITHIMS!     #####
+
+# LIST EXAMPLES AND LIST FUNCTIONS #                         
+
+courses = ['history', 'Math', 'Science', 'Physical Ed']
+print('first list',courses)
+
+# adding a value to our list
+courses.insert(0, 'Art')
+print('inserting item into our list', courses)
+
+# extending our list with asnother list
+courses_2 = ['Music', 'Detention']
+courses.extend(courses_2)
+print('extending a list with another list', courses)
+
+#removing items from our list
+courses.pop()
+print('removing an item from our list', courses)
+
+#reversing our list
+courses.reverse()
+print('reversing our list', courses)
+
+#sorting our list
+courses.sort()
+print('sorting our list', courses)
+
+#for loop for a list
+data = ['hello', 'cruel', 'world']
+for item in data:
+    print(item)
+for index, item in enumerate(data):
+    print(index, item)
+
+
+
+# TUPLE EXAMPLES AND FUNCTIONS #
+
+
+
